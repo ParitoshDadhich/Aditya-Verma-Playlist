ebe1321b481261a3a581c281f68d037dbf7e846d
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-05-30 04:27:09 +0530

    time o(n) || space o(n)
    Example 1:
    
    Input:
    N = 5
    arr[] = {1,2,3,4,5}
    Output: 6
    Explanation: Smallest positive missing 
    number is 6.
    
    Example 2:
    
    Input:
    N = 5
    arr[] = {0,-10,1,3,-20}
    Output: 2
    Explanation: Smallest positive missing 
    number is 2.

---
+++ Find Missing and Duplicate numbers from 1 to N/M1_smallestMissingPositiveNumber.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    //Function to find the smallest positive number missing from the array.
+    static int missingNumber(int arr[], int size)
+    {
+        HashSet<Integer> hs = new HashSet<>();
+        
+        for(int i: arr)
+            hs.add(i);
+        
+        int ans = -1;
+        for(int i=1; i<Integer.MAX_VALUE; i++){
+            if(!hs.contains(i)){
+                ans = i;
+                break;
+            }
+        }   
+        
+        return ans;
+    }
+}
