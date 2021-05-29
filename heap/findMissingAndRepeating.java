0e89357ccadf1448c56347b7a6cf9746f2690b14
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-05-30 02:50:28 +0530

    time o(n) || space o(1) || used swap sort concept
    Example 1:
    
    Input:
    N = 2
    Arr[] = {2, 2}
    Output: 2 1
    Explanation: Repeating number is 2 and 
    smallest positive missing number is 1.
    
    Example 2:
    
    Input:
    N = 3
    Arr[] = {1, 3, 3}
    Output: 3 2
    Explanation: Repeating number is 3 and 
    smallest positive missing number is 2.

---
+++ heap/findMissingAndRepeating.java
@@ -0,0 +1,30 @@
+
+class Solve {
+    int[] findTwoElement(int arr[], int n) {
+        int ans[] = new int[2];
+        
+        int i=0;
+        while(i<n){
+            if(arr[i] == arr[arr[i] -1])
+                i++;
+            else if(arr[i] != arr[arr[i] -1]){
+                int t = arr[i];
+                arr[i] = arr[arr[i]-1];
+                arr[t-1] = t;
+            }
+        }
+        
+        // for(int k: arr)
+        //     System.out.print(k + " ");
+        
+        for(i=0; i<n; i++){
+            if(arr[i] != i+1){
+                ans[0] = arr[i];
+                ans[1] = i+1;
+            }
+        }
+        
+        return ans;
+        
+    }
+}
